608bd3b90208c7aeed8de9a4aaaeeca6e96efe1b
Angel Concha Layme <aconchal@unsa.edu.pe>
2022-12-12 03:48:57 -0500

    Delete correct_6_006.py

--- sort/question_1/code/correct/correct_6_006.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort(arr):
-    cambios = True
-    while cambios:
-        cambios = False
-        for j in range(len(arr)-1):
-            arr[j], arr[j+1] = (arr[j+1], arr[j]) if arr[j] > arr[j+1] else (arr[j], arr[j+1])
-            cambios = True if arr[j] > arr[j+1] else cambios
-    return arr
